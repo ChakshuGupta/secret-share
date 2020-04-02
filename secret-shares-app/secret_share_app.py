9801a1931366835d319b289963087d456f3ff781
Chakshu <chakshugupta@gmail.com>
2020-04-02 13:45:42 +0200

    Update the UI
    Add Text browser to print shamir shares
    Add export button and radio buttons for options

--- secret-shares-app/secret_share_app.py
+++ secret-shares-app/secret_share_app.py
@@ -5,7 +5,7 @@ qtCreatorFile = "app_ui.ui"
  
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
  
-class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+class SecretShareApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
@@ -13,6 +13,6 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
  
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = MyApp()
+    window = SecretShareApp()
     window.show()
     sys.exit(app.exec_())
