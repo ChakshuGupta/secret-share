fc6d1b1938205cd01a0ad260c6fcd90c035801eb
Chakshu <chakshugupta@gmail.com>
2020-04-16 14:30:47 +0200

    Add Spanish language option
    Add spanish wordlist
    Add spanish option to the UI
    
    TODO: Add language option to the CLI and the API

--- wordlist/__init__.py
+++ wordlist/__init__.py
@@ -2,8 +2,10 @@
 from wordlist.wordlist_english import ENGLISH_WORDLIST
 from wordlist.wordlist_french import FRENCH_WORDLIST
 from wordlist.wordlist_italian import ITALIAN_WORDLIST
+from wordlist.wordlist_spanish import SPANISH_WORDLIST
 
 WORDLIST = dict()
 WORDLIST["english"] = ENGLISH_WORDLIST
 WORDLIST["french"] = FRENCH_WORDLIST
 WORDLIST["italian"] = ITALIAN_WORDLIST
+WORDLIST["spanish"] = SPANISH_WORDLIST
